28800344e9d7502ac98bdbd66786720543f55416
Anthony Rivas <kamikazebot@gmail.com>
2019-03-27 21:07:57 -0700

    Fixing scripts and updating PORT variable to be dynaming for deploy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express'),
     router = require('./routes');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 // Bodyparsing
